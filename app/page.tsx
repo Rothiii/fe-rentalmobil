5b3289f5c87ce3681e01a21ace7bc5f8d4ff95eb
unknown <drome.emord@gmail.com>
2025-08-27 16:12:16 +0800

    change name of variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import Camp from "@/components/Camp";
+import Why from "@/components/Why";
 import Features from "@/components/Features";
-import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Camp />
+      <Why />
       <Guide />
       <Features />
-      <GetApp />
     </>
   );
 }
