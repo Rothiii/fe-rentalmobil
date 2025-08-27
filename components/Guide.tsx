f58b98a893e792375b3e78efde0bc209df288d35
unknown <drome.emord@gmail.com>
2025-08-28 02:16:02 +0800

    adjust max width of tittle

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -13,8 +13,8 @@ const Guide = () => {
         <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
           Kami disini untuk Anda
         </p>
-        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+        <div className="flex flex-col flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64">
             Rute Perjalanan
           </h2>
           <p className="regular-16 text-gray-30 xl:max-w-[520px] text-justify">
