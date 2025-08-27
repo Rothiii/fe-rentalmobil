72b908b52b247b492c14055b69fe27b9da1d94ca
unknown <drome.emord@gmail.com>
2025-08-27 15:46:21 +0800

    delete bg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
-import Image from "next/image"
-import Button from "./Button"
+"use client";
+
+import Image from "next/image";
+import Button from "./Button";
 
 function Hero() {
   return (
@@ -7,8 +9,8 @@ function Hero() {
       <div className="hero-map"/>
       {/* LEFT */}
 
-      <div className="relative z-30 flex flex-1 flex-col xl:w-1/2">
-        <Image 
+      <div className="relative z-30 flex flex-1 flex-col xl:w-2/3">
+        <Image
           src="/camp.svg"
           alt="camp"
           width={50}
@@ -17,74 +19,72 @@ function Hero() {
         />
         <h1 className="bold-52 lg:bold-88"> Travel Kalimantan Timur</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-          Dapatkan layanan antar jemput yang cepat, nyaman, dan aman. Dan pengiriman barang dengan satu layanan terpercaya. Kecepatan, keamanan, dan kenyamanan dalam satu solusi. Kami hadir untuk memenuhi kebutuhan transportasi Anda.
+          Dapatkan layanan antar jemput yang cepat, nyaman, dan aman. Dan
+          pengiriman barang dengan satu layanan terpercaya. Kecepatan, keamanan,
+          dan kenyamanan dalam satu solusi. Kami hadir untuk memenuhi kebutuhan
+          transportasi Anda.
         </p>
 
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
-            {Array(5).fill(0).map((_, index) => (
-              <Image
-                src="/star.svg"
-                key={index}
-                alt="star"
-                width={24}
-                height={24}
-              />
-            ))}
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <Image
+                  src="/star.svg"
+                  key={index}
+                  alt="star"
+                  width={24}
+                  height={24}
+                />
+              ))}
           </div>
 
           <p className="bold-16 lg:bold-20 text-blue-70">
             5000+
-            <span className="regular-16 lg:regular-20 ml-1">Pengguna</span>
+            <span className="regular-16 lg:regular-20 ml-1">Pelanggan</span>
           </p>
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
-            <Button 
-              type="button"
-              title="Get Started"
-              variant="btn_green"
-            />
-            <Button 
-              type="button"
-              title="Cara kerja?"
-              icon="/play.svg"
-              variant="btn_white_text"
-            />
+          <Button
+            type="button"
+            title="Hubungi Sekarang"
+            variant="btn_green"
+            onClick={() => {
+              const phoneNumber = "6287841222233"; // Replace with your WhatsApp number
+              const message =
+                "Halo, saya tertarik dengan layanan Travel Kalimantan Timur. Bisa tolong berikan jadwal Travel/Jastip hari ini?";
+              const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+                message
+              )}`;
+              window.open(whatsappUrl, "_blank");
+            }}
+          />
         </div>
       </div>
-      
-      {/* RIGHT */}
-      <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
 
+      {/* RIGHT */}
+      {/* <div className="relative flex flex-1 items-start">
+        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
-              <p className="regular-16 text-gray-20"> Location </p>
-              <Image src="/close.svg" alt="close" width={24} height={24}/>
+              <p className="bold-20 text-gray-20"> Melayani Daerah </p>
+              <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Kalimantan Timur</p>
+            <ul className="flex flex-col gap-2 mt-6">
+              <li className="regular-16 text-white">Balikpapan</li>
+              <li className="regular-16 text-white">Samarinda</li>
+              <li className="regular-16 text-white">Bontang</li>
+              <li className="regular-16 text-white">Tanah Grogot</li>
+              <li className="regular-16 text-white">Banjarmasin</li>
+              <li className="regular-16 text-white">Dan Sekitarnya.</li>
+            </ul>
           </div>
-
-          <div className="flexBetween">
-            {/* <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Time</p>
-              <p className="bold-20 text-white">10am</p>
-            </div> */}
-            <div className="flex flex-col">
-              <Button 
-                type="button"
-                title="Book Now"
-                icon="/play.svg"
-                variant="btn_white_text"
-              />
-            </div>
-          </div>
-
         </div>
-      </div>
+      </div> */}
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
